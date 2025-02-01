e4153727eba814bf15e94e744219f2e6b0b4b81d
Gilvan Araujo <gilvanjunior@protonmail.com>
2025-02-01 12:50:48 -0300

    feat(config): global setup of react native elements and expo fonts

--- App.tsx
+++ App.tsx
@@ -1,18 +1,36 @@
-import { Text, View } from 'react-native';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  Roboto_900Black,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+import { defaultTheme } from '@rneui/base';
+import { ThemeProvider } from '@rneui/themed';
+import { ActivityIndicator, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Roboto_900Black,
+  });
+
+  if (!fontsLoaded) return <ActivityIndicator />;
+
   return (
     <SafeAreaProvider>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text>Universal React with Expo</Text>
-      </View>
+      <ThemeProvider theme={defaultTheme}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text>Universal React with Expo</Text>
+        </View>
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
