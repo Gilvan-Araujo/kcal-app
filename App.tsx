413e4fd6cb5da903cf71e8bb00370231e2cfea9c
Gilvan Araujo <gilvanjunior@protonmail.com>
2025-02-01 12:25:31 -0300

    build(rne): install react native elements

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <SafeAreaProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text>Universal React with Expo</Text>
+      </View>
+    </SafeAreaProvider>
   );
 }
