057ee147685e3979c4ba5f1a001a6eccf6328eb6
Gilvan Araujo <gilvanjunior@protonmail.com>
2025-02-10 11:42:25 -0300

    feat(routes): abstract routes to separate file and add new welcome page to navigator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { ActivityIndicator, StatusBar } from 'react-native';
 
 import {
+  Roboto_300Light,
   Roboto_400Regular,
   Roboto_700Bold,
   Roboto_900Black,
@@ -8,20 +9,17 @@ import {
 } from '@expo-google-fonts/roboto';
 import { RobotoMono_700Bold } from '@expo-google-fonts/roboto-mono';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { defaultTheme } from '@rneui/base';
 import { ThemeProvider } from '@rneui/themed';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { Home } from '@screens/home';
-import { Login } from '@screens/login';
-import { SignUp } from '@screens/sign-up';
-
 import { theme } from '@theme/theme';
 
-const Stack = createNativeStackNavigator();
+import { Routes } from './src/routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
+    Roboto_300Light,
     Roboto_400Regular,
     Roboto_700Bold,
     Roboto_900Black,
@@ -35,23 +33,8 @@ export default function App() {
       <NavigationContainer>
         <ThemeProvider theme={theme}>
           <StatusBar barStyle="light-content" />
-          <Stack.Navigator
-            id={undefined}
-            screenOptions={{
-              headerShown: false,
-              contentStyle: {
-                flex: 1,
-                backgroundColor: theme.darkColors?.background,
-                padding: 16,
-              },
-              animation: 'slide_from_bottom',
-            }}
-            initialRouteName="home"
-          >
-            <Stack.Screen name="home" component={Home} />
-            <Stack.Screen name="login" component={Login} />
-            <Stack.Screen name="signup" component={SignUp} />
-          </Stack.Navigator>
+
+          <Routes />
         </ThemeProvider>
       </NavigationContainer>
     </SafeAreaProvider>
---
+++ src/routes/index.tsx
@@ -0,0 +1,37 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useTheme } from '@rneui/themed';
+
+import { Home } from '@screens/home';
+import { Login } from '@screens/login';
+import { SignUp } from '@screens/sign-up';
+import { Welcome } from '@screens/welcome';
+
+const Stack = createNativeStackNavigator();
+
+export const Routes = () => {
+  const { theme } = useTheme();
+
+  return (
+    <>
+      <Stack.Navigator
+        id={undefined}
+        screenOptions={(props) => ({
+          headerShown: false,
+          contentStyle: {
+            flex: 1,
+            backgroundColor: theme.colors?.background,
+            padding: 16,
+            ...(props.route.name === 'welcome' && { padding: 0 }),
+          },
+          animation: 'slide_from_bottom',
+        })}
+        initialRouteName="home"
+      >
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="signup" component={SignUp} />
+        <Stack.Screen name="welcome" component={Welcome} />
+      </Stack.Navigator>
+    </>
+  );
+};
