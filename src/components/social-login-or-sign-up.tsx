10036d76dd62f834f4c9cb6a34fa4ed18542417a
Gilvan Araujo <gilvanjunior@protonmail.com>
2025-02-10 11:29:48 -0300

    refactor(social login): change svg behavior for light and dark modes

--- src/components/social-login-or-sign-up.tsx
+++ src/components/social-login-or-sign-up.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TouchableOpacity, View, ViewProps } from 'react-native';
 
-import { Text } from '@rneui/themed';
+import { Text, useTheme } from '@rneui/themed';
 
 import AppleLogo from '@assets/apple-logo.svg';
 import FacebookLogo from '@assets/facebook-logo.svg';
@@ -15,20 +15,24 @@ export const SocialLoginOrSignUp = ({
   variant,
   style,
   ...props
-}: SocialLoginOrSignUpProps) => (
-  <View style={[{ alignItems: 'center', gap: 20 }, style]} {...props}>
-    <Text smallText>Or</Text>
-    <Text smallText>{variant === 'login' ? 'Log in' : 'Sign up'} with</Text>
-    <View style={{ flexDirection: 'row', gap: 24 }}>
-      <TouchableOpacity>
-        <AppleLogo width={40} height={40} />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <GoogleLogo width={40} height={40} />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <FacebookLogo width={40} height={40} />
-      </TouchableOpacity>
+}: SocialLoginOrSignUpProps) => {
+  const { theme } = useTheme();
+
+  return (
+    <View style={[{ alignItems: 'center', gap: 20 }, style]} {...props}>
+      <Text smallText>Or</Text>
+      <Text smallText>{variant === 'login' ? 'Log in' : 'Sign up'} with</Text>
+      <View style={{ flexDirection: 'row', gap: 24 }}>
+        <TouchableOpacity>
+          <AppleLogo width={40} height={40} color={theme.colors.black} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <GoogleLogo width={40} height={40} color={theme.colors.black} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <FacebookLogo width={40} height={40} fill={theme.colors.black} />
+        </TouchableOpacity>
+      </View>
     </View>
-  </View>
-);
+  );
+};
